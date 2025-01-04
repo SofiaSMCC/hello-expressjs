13a704e5d2b7261da6e7ca6ff399b546da46e302
Sofia <sofia.salazar.m@utec.edu.pe>
2025-01-03 22:34:46 -0500

    feat(simple logger): add winston library to manage loggers

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import bodyParser from "body-parser";
 import movieRoutes from "./movie/movie.routes";
 import authRoutes from "./auth/auth.routes";
 import animeRoutes from "./anime/anime.routes";
+import logger from "./logger";
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use((req, _res, next) => {
+  logger.info(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use(bodyParser.json());
 
@@ -15,4 +21,6 @@ app.use("/auth", authRoutes);
 app.use("/movies", movieRoutes);
 app.use("/anime", animeRoutes);
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
