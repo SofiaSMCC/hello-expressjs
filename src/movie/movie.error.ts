1a4fbca2a01ac9164b192179f68091ada6a0a676
Sofia <sofia.salazar.m@utec.edu.pe>
2025-01-05 21:41:09 -0500

    refactor(movie): remove movie.error, it's unnecesary

--- src/movie/movie.error.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from "../types/error";
-
-export function throwError(message: string, status: number): never {
-  const error: CustomError = new Error(message);
-  error.status = status;
-  throw error;
-}
